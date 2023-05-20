206232c7e5bfb3815b2fc2c7616be4be0f7fe731
SergiyKochenko <sergiykochenko@gmail.com>
2023-05-20 22:55:01 +0000

    added e-mail confirmation and webhooks

--- jasmine_boutique/settings.py
+++ jasmine_boutique/settings.py
@@ -199,7 +199,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'jasmineboutique@example.com'
+    DEFAULT_FROM_EMAIL = 'sergiykochenko@gmail.com.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
